9b7fa815fc48139c3476a3e5ba09ba3fc30057fe
qambatali <q.ambatali@gmail.com>
2021-08-24 17:42:00 +1000

    repeated project

---
+++ DjangoNews/she_codes_news/news/forms.py
@@ -0,0 +1,15 @@
+
+from django import forms
+from django.forms import ModelForm
+from .models import NewsStory
+
+# allow Django to infer all the form fields and validation from a model
+class StoryForm(ModelForm):
+    # nested class we want Django to infer from
+    class Meta:
+        model = NewsStory
+        # list of fields to be included in the form
+        fields = ['title','pub_date', 'content']
+        widgets = {
+        	'pub_date': forms.DateInput(format=('%m/%d/%Y'), attrs={'class':'form-control', 'placeholder':'Select a date', 'type':'date'}),
+    	}
---
+++ DjangoNews/she_codes_news/news/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+app_name = 'news'
+
+urlpatterns = [
+    path('', views.IndexView.as_view(), name='index'), # /news/
+    path('<int:pk>/', views.StoryView.as_view(), name='story'), # /news/3
+    path('add-story/', views.AddStoryView.as_view(), name='newStory') # /news/add-story
+]
+ 
