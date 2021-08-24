3a148cc2c8543c669021904e83594b92a7974663
qambatali <q.ambatali@gmail.com>
2021-08-24 18:18:46 +1000

    Project progress

--- NewsDjango/she_codes_news/users/templates/users/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import CreateAccountView
-
-app_name = 'users'
-urlpatterns = [
-    path('create-account/', CreateAccountView.as_view(), name='createAccount'),
-]
